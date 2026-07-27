c6eb088dd2f91ce912db68c3eeb8718a5f016471
Md Abu Sayem Musa <143425981+abusayemmusa@users.noreply.github.com>
2026-07-28 02:20:32 +0600

    Update Basic_1.cpp

--- Basic_1.cpp
+++ Basic_1.cpp
@@ -22,5 +22,23 @@ int main() {
     return 0;
 }
 ========================================================================================================
+# variable
 ========================================================================================================
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int age = 20;
+    float height = 5.9;
+    char gender = 'M';
+    bool isStudent = true;
+    double weight = 70.5;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Gender: " << gender << endl;
+    cout << "Is Student: " << isStudent << endl;
+    cout << "Weight: " << weight << endl;
+    return 0;
+}
 ========================================================================================================
